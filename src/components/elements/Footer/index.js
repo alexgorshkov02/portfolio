dd2cd67d247ad35e702bf81e9aad93230070a982
Alex <82488910+alexgorshkov02@users.noreply.github.com>
2021-09-06 21:41:46 -0400

    Implemented the footer. Issue #12

--- src/components/elements/Footer/index.js
+++ src/components/elements/Footer/index.js
@@ -1,10 +1,29 @@
 import React from "react";
-import { Header, Segment } from "semantic-ui-react";
+import { Header, Segment, Grid, Icon } from "semantic-ui-react";
+import "./index.css";
 
 const Footer = () => (
-  <Segment>
-    <Header as="h3" textAlign="center">
-    Footer
+  <Segment id="footer">
+    <Header as="h1" textAlign="center">
+      <Grid centered relaxed >
+        <Grid.Row >
+          <Grid.Column>
+          <a href="mailto:alexgorshkov0290@gmail.com">
+              <Icon link inverted name="mail" size="large" />
+            </a>
+          </Grid.Column>
+          <Grid.Column>
+            <a href="https://github.com/alexgorshkov02">
+              <Icon link inverted name="github" size="large" />
+            </a>
+          </Grid.Column>
+          <Grid.Column>
+            <a href="https://www.linkedin.com/in/alexander-gorshkov-212915119/">
+              <Icon link inverted name="linkedin square" size="large" />
+            </a>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     </Header>
   </Segment>
 );
