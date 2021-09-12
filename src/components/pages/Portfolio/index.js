65388355d9742df14f74d53383c72ced1ccb608b
Alex <82488910+alexgorshkov02@users.noreply.github.com>
2021-09-12 01:26:54 -0400

    Added screenshots for the projects and dimmers for them

--- src/components/pages/Portfolio/index.js
+++ src/components/pages/Portfolio/index.js
@@ -1,19 +1,98 @@
-import React from "react";
+import React, { Component } from "react";
+import { Image, Dimmer, Header, Segment, Icon } from "semantic-ui-react";
 
-const Portfolio = () => (
-  <div>
-    <h1>Portfolio Page</h1>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-      molestiae quas vel sint commodi repudiandae consequuntur voluptatum
-      laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga
-      praesentium optio, eaque rerum! Provident similique accusantium nemo
-      autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto
-      voluptate aliquam nihil, eveniet aliquid culpa officia aut! Impedit sit
-      sunt quaerat, odit, tenetur error, harum nesciunt ipsum debitis quas
-      aliquid.
-    </p>
-  </div>
-);
+export default class Portfolio extends Component {
+  state = {};
 
-export default Portfolio;
+  handleShow = () => this.setState({ active: true });
+  handleHide = () => this.setState({ active: false });
+
+  render() {
+    const { active } = this.state;
+    const content1 = (
+      <div>
+        <h2><a href="https://alexgorshkov02.github.io/SteamAPIproject">DealSella</a>
+        <a href="https://github.com/alexgorshkov02/SteamAPIproject">
+              <Icon link inverted name="github" size="big" />
+            </a></h2>
+        <Header as="h4" inverted>
+          HTML/CSS/JavaScript
+        </Header>
+      </div>
+    );
+
+    const content2 = (
+      <div>
+        <h2><a href="https://alexgorshkov02.github.io/SteamAPIproject">DealSella</a>
+        <a href="https://github.com/alexgorshkov02/SteamAPIproject">
+              <Icon link inverted name="github" size="big" />
+            </a></h2>
+        <Header as="h4" inverted>
+          HTML/CSS/JavaScript
+        </Header>
+      </div>
+    );
+
+
+    return (
+      <Segment>
+        <h1>Portfolio</h1>
+        <Dimmer.Dimmable
+          as={Image}
+          dimmed={active}
+          dimmer={{ active, content1 }}
+          onMouseEnter={this.handleShow}
+          onMouseLeave={this.handleHide}
+          size="medium"
+          src="./assets/images/projectPreviews/dealSella.jpg"
+        />
+        <Dimmer.Dimmable
+          as={Image}
+          dimmed={active}
+          dimmer={{ active, content2 }}
+          onMouseEnter={this.handleShow}
+          onMouseLeave={this.handleHide}
+          size="medium"
+          src="./assets/images/projectPreviews/olympicNews.jpg"
+        />
+      </Segment>
+    );
+  }
+}
+
+// const Portfolio = () => (
+//   <>
+//   <Card>
+//     <Image
+//       src="./assets/images/projectPreviews/dealSella.jpg"
+//       wrapped
+//       ui={false}
+//     />
+//   </Card>
+
+// <Card>
+// <Image
+//   src={'./assets/images/projectPreviews/olympicNews.jpg'}
+//   wrapped
+//   ui={false}
+// />
+// <Card.Content>
+//   <Card.Header>Matthew</Card.Header>
+//   <Card.Meta>
+//     <span className="date">Joined in 2015</span>
+//   </Card.Meta>
+//   <Card.Description>
+//     Matthew is a musician living in Nashville.
+//   </Card.Description>
+// </Card.Content>
+// <Card.Content extra>
+//   <a>
+//     <Icon name="user" />
+//     22 Friends
+//   </a>
+// </Card.Content>
+// </Card>
+// </>
+// );
+
+// export default Portfolio;
